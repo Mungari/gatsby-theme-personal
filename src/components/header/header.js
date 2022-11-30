c95817b023f346a67dba51274dbd47f59dd149b7
Birbatron <mungarifr@gmail.com>
2022-11-30 11:10:50 +0100

    Trying passing contact as parameter

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,7 +2,7 @@ import React from "react"
 import { FaEnvelope } from "react-icons/fa"
 import { ProfileType } from "../../types"
 
-const Header = ({ initials }) => (
+const Header = ({ initials }, {contact}) => (
   <header className="flex justify-between p-4 lg:px-8">
     <span className="inline-flex w-14 h-14 lg:mt-4 font-header font-bold text-xl justify-center items-center text-center text-front border-2 border-solid border-front rounded-full">
       {initials}
--- src/types/profile.js
+++ src/types/profile.js
@@ -3,15 +3,8 @@ import { arrayOf, bool, number, shape, string, object } from "prop-types"
 
 export const ProfileType = {
   about: string.isRequired,
-  budget: shape({
-    currency: string.isRequired,
-    default: number.isRequired,
-    max: number.isRequired,
-    min: number.isRequired,
-  }).isRequired,
   company: string.isRequired,
   focus: string.isRequired,
-  focus_url: string,
   for_hire: bool.isRequired,
   image: shape({
     childImageSharp: object.isRequired,
